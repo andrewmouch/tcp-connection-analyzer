93496fc0c3c44c254e04bdd3deb2786fb37c2ae6
Andrew <andrewmouchantaf@gmail.com>
2026-01-19 17:55:59 -0500

    Added fields to tcp_result_t

--- include/tcp.h
+++ include/tcp.h
@@ -1,9 +1,21 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 typedef struct {
-    int hey;
+    uint16_t source_port;
+    uint16_t dest_port;
+    uint32_t seq_number;
+    uint32_t ack_number;
+
+    bool flag_syn; 
+    bool flag_ack; 
+    bool flag_fin; 
+    bool flag_rst; 
+    bool flag_psh; 
+    bool flag_urg; 
 } tcp_result_t;
 
 int parse_tcp_header(const unsigned char* packet, size_t len, tcp_result_t* out);
