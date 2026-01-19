accda32103e720feef0e5f8f1edb59b8798e18c9
Andrew Mouchantaf <39502987+andrewmouch@users.noreply.github.com>
2026-01-19 14:22:07 -0500

    Fix byte ordering of tcp printing (#16)

--- src/tcp.c
+++ src/tcp.c
@@ -3,13 +3,14 @@
 #include "tcp.h"
 #include <inttypes.h>
 #include <netinet/tcp.h>
+#include <arpa/inet.h>
 
 int parse_tcp_header(const unsigned char* packet, size_t len, tcp_result_t* out) {
     struct tcphdr *tcp_header = (struct tcphdr*) (packet);
-    printf("          TCP Source Port: %" PRIu16 "\n", tcp_header->th_sport);
-    printf("          TCP Destination Port: %" PRIu16 "\n", tcp_header->th_dport);
-    printf("          TCP Sequence Number: %" PRIu32 "\n", tcp_header->th_seq);
-    printf("          TCP Acknowledgement Number: %" PRIu32 "\n", tcp_header->th_ack);
+    printf("          TCP Source Port: %" PRIu16 "\n", ntohs(tcp_header->th_sport));
+    printf("          TCP Destination Port: %" PRIu16 "\n", ntohs(tcp_header->th_dport));
+    printf("          TCP Sequence Number: %" PRIu32 "\n", ntohl(tcp_header->th_seq));
+    printf("          TCP Acknowledgement Number: %" PRIu32 "\n", ntohl(tcp_header->th_ack));
 
     return 0;
 }
