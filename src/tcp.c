103fd2c7fbe22c0a391a02a7fe649c9ba3d1995e
Andrew <andrewmouchantaf@gmail.com>
2026-01-19 15:01:19 -0500

    Validate TCP segment size before parsing header

--- src/tcp.c
+++ src/tcp.c
@@ -5,8 +5,12 @@
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 
-int parse_tcp_header(const unsigned char* packet, size_t len, tcp_result_t* out) {
-    struct tcphdr *tcp_header = (struct tcphdr*) (packet);
+int parse_tcp_header(const unsigned char* segment, size_t len, tcp_result_t* out) {
+    if (len < sizeof(struct tcphdr)) {
+        fprintf(stderr, "Segment too small to contain a tcp header");
+        return -1;
+    }
+    struct tcphdr *tcp_header = (struct tcphdr*) (segment);
     printf("          TCP Source Port: %" PRIu16 "\n", ntohs(tcp_header->th_sport));
     printf("          TCP Destination Port: %" PRIu16 "\n", ntohs(tcp_header->th_dport));
     printf("          TCP Sequence Number: %" PRIu32 "\n", ntohl(tcp_header->th_seq));
